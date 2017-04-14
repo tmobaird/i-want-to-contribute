85dbf94895a1952c2b09f8f10fbc04c95a8917c4
Thomas Baird <tmobaird@gmail.com>
2017-04-14 00:02:43 -0400

    Refactoring to use stateless functional components (#7)

--- src/components/InformationLabel.jsx
+++ src/components/InformationLabel.jsx
@@ -2,24 +2,24 @@ import React, { PropTypes } from 'react';
 import BsSpecialLabel from './BsSpecialLabel';
 import '../styles/information-label.css';
 
-const quickInfoPropTypes = {
-  level: PropTypes.string
-};
-
-export default class InformationLabel extends React.Component {
-  static propTypes = quickInfoPropTypes;
-
-  render() {
-    if(this.props.data != null) {
-      return (
-        <span className='information-label'>
-          <BsSpecialLabel level={this.props.level} color={this.props.color}>
-            <strong>{ this.props.children }{ this.props.data }</strong>
-          </BsSpecialLabel>
-        </span>
-      );
-    } else {
-      return null;
-    }
+const InformationLabel = (props) => {
+  if(props.data) {
+    return (
+      <span className='information-label'>
+        <BsSpecialLabel level={props.level} color={props.color}>
+          <strong>{ props.children }{ props.data }</strong>
+        </BsSpecialLabel>
+      </span>
+    );
+  } else {
+    return null;
   }
 }
+
+InformationLabel.propTypes = {
+  level: PropTypes.string,
+  color: PropTypes.string,
+  data: PropTypes.node
+};
+
+export default InformationLabel;
--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router';
 const NotFoundPage = () => {
   return (
     <div>
-      <h4>
-        404 Page Not Found
-      </h4>
+      <h4>404 Page Not Found</h4>
       <Link to="/"> Go back to homepage </Link>
     </div>
   );
