ef09bbc8f2b82ef07deede9e35e5a0de59edfcd6
Thomas Baird <tbaird@mail.umw.edu>
2017-04-22 14:52:55 -0400

    Updating navbar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link, IndexLink } from 'react-router';
+import { Row } from 'react-bootstrap';
+import '../styles/navigation-bar.css';
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <Row className="NavigationBar">
+          <IndexLink to="/" className="navbar-link" activeClassName="active">Home</IndexLink>
+          {' | '}
+          <Link to="/about" className="navbar-link" activeClassName="active">About</Link>
+      </Row>
+    );
+  }
+}
+
+export default NavigationBar;
