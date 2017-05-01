977ca7764e544eb2d5604c79aa88c4c086afeeaa
Thomas Baird <tbaird@mail.umw.edu>
2017-05-01 00:33:20 -0400

    Adding emojis to contributor map

--- src/components/contributors-map/__tests__/MapMarkerPopupInformation.test.js
+++ src/components/contributors-map/__tests__/MapMarkerPopupInformation.test.js
@@ -13,6 +13,7 @@ describe('MapMarkerPopupInformation', () => {
   describe('with props', () => {
     const location = {
       "name": "Virginia",
+      "emoji": "test_emoji",
       "latitude": 37.4316,
       "longitude": -78.6569,
       "contributors": []
@@ -23,7 +24,14 @@ describe('MapMarkerPopupInformation', () => {
       expect(wrapper.find("div.map-marker-popup-information").exists()).toBe(true);
     });
     it('displays name', () => {
-      expect(wrapper.find("h5").text()).toEqual("Virginia");
+      expect(wrapper.find("span.location-name").text()).toEqual("Virginia ");
+    });
+    it('includes emojify', () => {
+      expect(wrapper.find("Emojify")).toHaveLength(1);
+    });
+    it('displays emoji', () => {
+      const emojiContainer = wrapper.find("Emojify");
+      expect(emojiContainer.contains(<span>:test_emoji:</span>)).toBe(true);
     });
     it('includes ContributorList', () => {
       expect(wrapper.find("ContributorList")).toHaveLength(1);
