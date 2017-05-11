7ae590b408c84d61e7e84273637e5cfa597f8bb8
Thomas Baird <tbaird@mail.umw.edu>
2017-05-11 19:21:59 -0400

    Colorizing output of server running on port output

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const colors = require('colors');
+colors.enabled = true; // enables colors so they show in foreman console
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  console.log(`Running on port ${PORT}`)
+  console.log(`Running on port ${PORT}`.green)
 })
