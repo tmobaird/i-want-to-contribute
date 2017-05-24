e8276a4f55d3dd89c723e5e274d30c6622ce0f22
Ricardo-Silva91 <ricardos897@hotmail.com>
2017-05-24 18:39:58 +0000

    fix for issue #51 - Add 'Find me on Github' Link to Navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 import { Row } from 'react-bootstrap';
 import '../styles/navigation-bar.css';
+import githubLogo from '../images/GitHub-Mark-32px.png';
 
 class NavigationBar extends Component {
   render() {
@@ -12,6 +13,7 @@ class NavigationBar extends Component {
         <Link to="/about" className="navbar-link" activeClassName="active">About</Link>
         {' | '}
         <Link to="/contributors" className="navbar-link" activeClassName="active">Contributors</Link>
+        <a href="https://github.com/tmobaird/i-want-to-contribute" className="navbar-link navbar-link-right">Find me on Github <img alt="Github" src={githubLogo}></img></a>
       </Row>
     );
   }
