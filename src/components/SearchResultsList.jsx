0e4645505a7375db9527fb2b8282cebaf1f62ac2
Devin Osorio <me@devinosor.io>
2017-08-16 13:29:52 -0700

    adds a stagger animation to each search result component

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import TweenMax from 'gsap';
 import SearchResult from './SearchResult';
 
 export default class SearchResultsList extends React.Component {
+  componentDidMount() {
+    // eslint-disable-next-line
+    TweenMax.staggerFrom('.animate', 1, { scale: 0.5, opacity: 0, delay: 0.5, ease: Linear.easeOut }, 0.2);
+  }
   render() {
     const { results, sortedIds, getAdditionalInfo } = this.props
 
     const sortedResults = sortedIds.map((id) => {
-      return <SearchResult key={id} data={results[id]} getAdditionalInfo={getAdditionalInfo} />
+      return (
+        <div key={id} className="animate">
+          <SearchResult
+            data={results[id]}
+            getAdditionalInfo={getAdditionalInfo}
+          />
+        </div>
+      )
     });
 
     return (
