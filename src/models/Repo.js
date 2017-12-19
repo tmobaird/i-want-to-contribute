d0600cf171cc050b3866835602783572c0e8f3fa
Thomas Baird <tbaird@mail.umw.edu>
2017-12-19 00:43:51 -0500

    Adding flow typed for jest and updating repo class

--- src/models/Repo.js
+++ src/models/Repo.js
@@ -3,38 +3,41 @@ import AdditionalInformation from './AdditionalInformation';
 import {License} from '../serializers/RepoSerializer';
 import ContributingInformation from "./ContributingInformation";
 
-interface RepoAttributes {
-  contributorsUrl: string;
-  createdAt: string;
-  description: string;
-  forks: number;
-  forksCount: number;
-  forksUrl: string;
-  fullName: string;
-  gitUrl: string;
-  hasIssues: boolean;
-  hasPages: boolean;
-  homepage: string;
-  htmlUrl: string;
-  id: number;
-  issuesUrl: string;
-  language: string;
-  license: License;
-  name: string;
-  openIssues: number;
-  openIssuesCount: number;
-  score: number;
-  sshUrl: string;
-  stargazersCount: number;
-  updatedAt: string;
-  url: string;
-  watchers: number;
-  watchersCount: number;
+type RepoAttributes = {
+  contributorsUrl?: string,
+  createdAt?: string,
+  description?: string,
+  forks?: number,
+  forksCount?: number,
+  forksUrl?: string,
+  fullName?: string,
+  gitUrl?: string,
+  hasIssues?: boolean,
+  hasPages?: boolean,
+  homepage?: string,
+  htmlUrl?: string,
+  id?: number,
+  issuesUrl?: string,
+  language?: string,
+  license?: License,
+  name?: string,
+  openIssues?: number,
+  openIssuesCount?: number,
+  score?: number,
+  sshUrl?: string,
+  stargazersCount?: number,
+  updatedAt?: string,
+  url?: string,
+  watchers?: number,
+  watchersCount?: number,
+  additionalInformation?: AdditionalInformation,
+  contributingInformation?: ContributingInformation
 }
 
 export default class Repo {
+  additionalInformation: AdditionalInformation;
   contributorsUrl: string;
-  contributingInformation: ContributingInformation | null;
+  contributingInformation: ContributingInformation;
   createdAt: string;
   description: string;
   forks: number;
@@ -60,9 +63,17 @@ export default class Repo {
   url: string;
   watchers: number;
   watchersCount: number;
-  additionalInformation: AdditionalInformation | null;
 
   constructor(data: RepoAttributes) {
-    Object.assign(this, data)
+    Object.assign(this, data);
+  }
+
+  static create(data: RepoAttributes): Repo {
+    return new Repo(data);
+  }
+
+  update(data: RepoAttributes): Repo {
+    const params = Object.assign({}, {...this}, data);
+    return new Repo(params);
   }
 }
--- src/models/Repo.test.js
+++ src/models/Repo.test.js
@@ -1,13 +1,44 @@
+// @flow
 import Repo from './Repo';
 
 describe('Repo', () => {
   it('is new-able', () => {
     const subject = new Repo({
+      thomas: 'test',
       id: 123,
       fullName: 'facebook/react',
       name: 'react',
       language: 'JavaScript'
     });
     expect(subject.fullName).toEqual('facebook/react')
+  });
+
+  describe('create', () => {
+    it('creates a new Repo', () => {
+      const subject = Repo.create({
+        id: 123,
+        fullName: 'facebook/react',
+        name: 'react',
+        language: 'JavaScript'
+      });
+
+      expect(subject instanceof Repo).toBeTruthy();
+    });
+  });
+  describe('update', () => {
+    let repo;
+    beforeEach(() => {
+      repo = Repo.create({id: 123, fullName: 'facebook/react', name: 'react'});
+    });
+
+    it('returns a new repo', () => {
+      const subject = repo.update({fullName: 'facebookCode/react'});
+      expect(subject instanceof Repo).toBeTruthy();
+    });
+
+    it('updates attributes', () => {
+      const subject = repo.update({fullName: 'facebookCode/react'});
+      expect(subject.fullName).toEqual('facebookCode/react');
+    });
   });
 });
