55c5bb799ec43bf2d8a343828a0d7b495f8f3f69
Thomas Baird <tbaird@mail.umw.edu>
2018-01-21 19:36:51 -0500

    Adding tooltip with textual information for status fetching

--- src/components/StatusDot.jsx
+++ src/components/StatusDot.jsx
@@ -1,16 +1,30 @@
 // @flow
 import * as React from 'react';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 import '../styles/StatusDot.css';
+
+const tooltip = (text: string) => (
+    <Tooltip id="status-tooltip">
+      {text}
+    </Tooltip>
+);
 
 const StatusDot = ({status}: { status: number }) => {
   let backgroundColor = '#FF2020';
+  let statusText = 'Sorry. We are currently experiencing outages. Some features may not work because of this.';
   if (status > 0) {
     backgroundColor = '#1EEE17';
+    statusText = 'All systems operational';
   } else if (status === 0) {
     backgroundColor = '#FFDA20';
+    statusText = 'Checking status...'
   }
 
-  return <div className='status-dot' style={{backgroundColor}}/>;
+  return (
+      <OverlayTrigger placement="bottom" overlay={tooltip(statusText)}>
+        <div className='status-dot' style={{backgroundColor}}/>
+      </OverlayTrigger>
+  );
 };
 
 export default StatusDot;
--- src/components/StatusDot.test.js
+++ src/components/StatusDot.test.js
@@ -12,17 +12,32 @@ describe(`StatusDot`, () => {
       const subject = shallow(<StatusDot status={-1}/>);
       expect(subject.find('div').props().style.backgroundColor).toEqual(red);
     });
+    it('includes tooltip with failure status', () => {
+      const subject = shallow(<StatusDot status={-1}/>);
+      const tooltipContent = subject.find('OverlayTrigger').props().overlay.props.children;
+      expect(tooltipContent).toEqual('Sorry. We are currently experiencing outages. Some features may not work because of this.');
+    });
   });
   describe('when status is == 0', () => {
     it('displays orange dot', () => {
       const subject = shallow(<StatusDot status={0}/>);
       expect(subject.find('div').props().style.backgroundColor).toEqual(yellow);
     });
+    it('displays checking... status message', () => {
+      const subject = shallow(<StatusDot status={0}/>);
+      const tooltipContent = subject.find('OverlayTrigger').props().overlay.props.children;
+      expect(tooltipContent).toEqual('Checking status...');
+    });
   });
   describe('when status is > 0', () => {
     it('displays green dot', () => {
       const subject = shallow(<StatusDot status={1}/>);
       expect(subject.find('div').props().style.backgroundColor).toEqual(green);
+    })
+    it('displays all systems operational status message', () => {
+      const subject = shallow(<StatusDot status={1}/>);
+      const tooltipContent = subject.find('OverlayTrigger').props().overlay.props.children;
+      expect(tooltipContent).toEqual('All systems operational');
     });
   });
 });
